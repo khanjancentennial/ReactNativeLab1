5892cd558bfeae0f057b8679fe36258c4e7510bb
khanjancentennial <khanjancentennial@gmail.com>
2023-09-06 12:26:20 -0400

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontWeight: "bold",fontSize:32}}>About Me</Text>
+      <Text style={{fontSize:20}}>Khanjan Dave</Text>
+      <Text style={{fontSize:20}}>Id: 301307330</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0AFFFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
